40361eedb1eaaa5b0ef4b83ee01e10443e82adb6
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-09 19:42:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,7 +19,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age <= 20) {
+  if (age >= 20) {
     return 'You are a grownup';
   } else if (age > 12) {
     return 'You are a kid';
