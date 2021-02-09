9559cc05fa4813280b50e9181b841b9bb01c3611
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-09 19:39:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,7 @@
 function basicTeenager (age) {
-  if (age > 13) {
+  if (age < 13) {
     return undefined;
-  } else if (age <= 19) {
+  } else if (age >= 19) {
     return undefined; 
   } else {
     return 'You are a teenager!';
