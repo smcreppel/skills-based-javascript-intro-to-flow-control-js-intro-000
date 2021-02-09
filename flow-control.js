4383d7700eac5bea5b076bf6a70b83c8237b91de
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-09 20:02:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,34 +7,42 @@ function basicTeenager (age) {
 }
 
 function teenager(age) {
-  if (age <= 13) {
-    return undefined;
-  } else if (age >= 19) {
-    return undefined;
+  if (age < 13) {
+    return 'You are not a teenager';
+  } else if (age > 19) {
+    return 'You are not a teenager';
   }else {
     return 'You are a teenager!';
   }
 }
 
 function ageChecker(age) {
-  if (age >= 20) {
-    return 'You are a grownup';
-  } else if (age < 12) {
+  if (age < 13) {
     return 'You are a kid';
+  } else if (age > 19) {
+    return 'You are a grownup';
+  } else {
+    return 'You are a teenager!';
   }
-
 }
 
 function ternaryTeenager(age) {
-  if (age >= 12) {
-    return 'You are a kid.';
-  } else if (age <= 19) {
-    return undefined;
-  } else {
-    return 'You are not a teenager';
-  }
+  return age < 13 || age > 19 ?'You are not a teenager':'You are a teenager'
   }
 
 function switchAge(age) {
-
+  console.log (age)
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:    
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return 'You are a teenager';
+    break;
+    default:
+    return 'You have an age';
+  }
 }
