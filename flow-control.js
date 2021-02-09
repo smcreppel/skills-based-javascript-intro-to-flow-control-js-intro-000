cc02b4d22fb5e1b45f10995eee523017de05ffdb
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-09 17:47:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,15 +2,15 @@ function basicTeenager (age) {if (age >19) 'You are a teenager!';
 
 }
 
-function teenager(age) {
+function teenager(age) {if else (age 13-19) 'You are a Teenager!';
 
 }
 
-function ageChecker(age) {
+function ageChecker(age) {else (age 19<) 'You are not a Teenager!';
 
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {else (age 13-19) 'You are a teenager'
 
 }
 
